18003e023595d37e006f07cc28cf940064367bd1
Anis <anis.kraiem1996@gmail.com>
2021-03-29 17:58:59 +0100

    before merge

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -5,91 +5,91 @@ namespace App\Controller;
 use App\Entity\Categorie;
 use App\Form\CategorieType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/societe={idSociete}/categorie={idCategorie}", name="afficherCategorie")
+     * @Route("/ajouCategorie", name="ac")
      */
-    public function afficherCategorie($idSociete,$idCategorie): Response
+    public function addCat(Request $request)
     {
-        return null;
-    }
+        $cat = new Categorie();
 
-    /**
-     * @Route("/categorie", name="afficherToutCategorie")
-     */
-    public function afficherToutCategorie(): Response
-    {
-        return $this->render('/frontEnd/utilisateur/societe/categorie/afficherCategorie.html.twig', [
-            'categories' => $this->getDoctrine()->getManager()->getRepository(Categorie::class)->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/societe/categorie/ajouter", name="ajouterCategorie")
-     */
-    public function ajouterCategorie(Request $request)
-    {
-        $categorie = new Categorie();
-
-        $form = $this->createForm(CategorieType::class, $categorie)
+        $form = $this->createForm(CategorieType::class, $cat)
             ->add('Ajouter', SubmitType::class)
             ->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($cat);
+            $em->flush();
 
-            $categorie = $form->getData();
 
-            $categorieRepository = $this->getDoctrine()->getManager();
-            $categorieRepository->persist($categorie);
-            $categorieRepository->flush();
-
-            return $this->redirectToRoute('afficherCategorie');
+            return $this->redirectToRoute('listc');
         }
-
-        return $this->render('/frontEnd/utilisateur/societe/categorie/manipulerCategorie.html.twig', [
+        return $this->render('/backEnd/ajouCategorie.html.twig', [
             'form' => $form->createView(),
-            'manipulation' => "Modifier",
+            'Cat' => $this->getDoctrine()->getRepository(Categorie::class)->findAll()
+
         ]);
     }
 
     /**
-     * @Route("/societe/categorie={idCategorie}/modifier", name="modifierCategorie")
+     * @Route("/deletecat/{id}", name="dele")
      */
-    public function modifierCategorie(Request $request, $idCategorie)
+    public function delCat(Request $request, $id)
     {
-        $categorieRepository = $this->getDoctrine()->getManager();
-        $categorie = $categorieRepository->getRepository(Categorie::class)->find($idCategorie);
+        $em = $this->getDoctrine()->getManager();
+        $e = $em->getRepository(Categorie::class)->find($id);
+        $em->remove($e);
+        $em->flush();
 
-        $form = $this->createForm(CategorieType::class, $categorie);
-        $form->add('Modifier', SubmitType::class);
-        $form->handleRequest($request);
+        return $this->redirectToRoute('listc');
+    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $categorieRepository->flush();
-            return $this->redirectToRoute('afficherCategorie');
+    /**
+     * @Route("/modif/{id}", name="modcat")
+     */
+    public function ModifierCat(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $cat = $em->getRepository(Categorie::class)->find($id);
+
+        $form = $this->createForm(CategorieType::class, $cat)
+            ->add('Modifier', SubmitType::class)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('listc');
         }
 
-        return $this->render('/frontEnd/utilisateur/societe/categorie/manipulerCategorie.html.twig', [
-            'form' => $form->createView(),
-            'manipulation' => "Modifier",
+        return $this->render('/backEnd/ajouCategorie.html.twig', ['form' => $form->createView(),
+            'Cat' => $this->getDoctrine()->getRepository(Categorie::class)->findAll()
         ]);
     }
 
     /**
-     * @Route("/societe/categorie={idCategorie}/supprimer", name="supprimerCategorie")
+     * @Route("/listcat", name="listc")
      */
-    public function supprimerCategorie($idCategorie)
+    public function LireCat(Request $request)
     {
-        $manager = $this->getDoctrine()->getManager();
-        $categorie = $manager->getRepository(Categorie::class)->find($idCategorie);
-        $manager->remove($categorie);
-        $manager->flush();
-        return $this->redirectToRoute('afficherCategorie');
+        $em = $this->getDoctrine()->getManager();
+        $cat = $em->getRepository(Categorie::class)->findAll();
+
+
+        return $this->render('/backEnd/listcat.html.twig', [
+            'Cat' => $cat
+        ]);
     }
+
+
 }
--- src/Form/OffreDeTravailType.php
+++ src/Form/OffreDeTravailType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Categorie;
-use App\Entity\OffreDeTravail;
-use App\Entity\Societe;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use App\Entity\OffreDeTravail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,14 +17,11 @@ class OffreDeTravailType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('job')
             ->add('description')
             ->add('categorie', EntityType::class, [
                 'class' => Categorie::class,
-                'choice_label' => 'id',
-                'multiple' => false])
-            ->add('societe', EntityType::class, [
-                'class' => Societe::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nomCategorie',
                 'multiple' => false]);
     }
 
@@ -32,4 +31,5 @@ class OffreDeTravailType extends AbstractType
             'data_class' => OffreDeTravail::class,
         ]);
     }
+
 }
