e8daaa0ed5d56dbf597aef709db1c64a014b3cc3
Faten <faten.sidhom@esprit.tn>
2021-03-30 09:28:44 +0100

    fixes

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -50,7 +50,7 @@ class BaseController extends AbstractController
                                 'emailUtilisateur' => $utilisateurConnexion->getEmail(),
                                 'idCandidat' => $utilisateurConnexion->getCandidat()->getId(),
                             ]);
-                            return $this->redirectToRoute("publication");
+                            return $this->redirectToRoute("afficherPublication");
                         } else {
                             $this->session->set("utilisateur", [
                                 'idUtilisateur' => $utilisateurConnexion->getId(),
